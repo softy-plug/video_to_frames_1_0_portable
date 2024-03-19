3f842327b47c07fd75de02baccd22748cd4f7eff
Softy Plug <132814762+softy-plug@users.noreply.github.com>
2024-03-19 15:11:14 +0300

    video_to_frames_1_0
    video_to_frames_1_0 - utility for create frames / images from video files using ffmpeg console

---
+++ video_to_frames_1_0_portable/png/00_video_to_frames_1_0_png.bat
@@ -0,0 +1,102 @@
+::[FFmpeg video_to_frames_1_0 softy_plug]
+@echo off
+
+:again
+
+mkdir out_frames
+for %%i in (*.mp4) do (
+ffmpeg -i "%%i" "out_frames/%%~ni.png"
+)
+
+for %%i in (*.avi) do (
+ffmpeg -i "%%i" "out_frames/%%~ni.png"
+)
+
+for %%i in (*.mkv) do (
+ffmpeg -i "%%i" "out_frames/%%~ni.png"
+)
+
+for %%i in (*.mov) do (
+ffmpeg -i "%%i" "out_frames/%%~ni.png"
+)
+
+for %%i in (*.wmv) do (
+ffmpeg -i "%%i" "out_frames/%%~ni.png"
+)
+
+for %%i in (*.flv) do (
+ffmpeg -i "%%i" "out_frames/%%~ni.png"
+)
+
+for %%i in (*.avchd) do (
+ffmpeg -i "%%i" "out_frames/%%~ni.png"
+)
+
+for %%i in (*.f4v) do (
+ffmpeg -i "%%i" "out_frames/%%~ni.png"
+)
+
+for %%i in (*.swf) do (
+ffmpeg -i "%%i" "out_frames/%%~ni.png"
+)
+
+for %%i in (*.mpv) do (
+ffmpeg -i "%%i" "out_frames/%%~ni.png"
+)
+
+for %%i in (*.mpe) do (
+ffmpeg -i "%%i" "out_frames/%%~ni.png"
+)
+
+for %%i in (*.mp2) do (
+ffmpeg -i "%%i" "out_frames/%%~ni.png"
+)
+
+for %%i in (*.mpeg) do (
+ffmpeg -i "%%i" "out_frames/%%~ni.png"
+)
+
+for %%i in (*.mpg) do (
+ffmpeg -i "%%i" "out_frames/%%~ni.png"
+)
+
+for %%i in (*.3gp) do (
+ffmpeg -i "%%i" "out_frames/%%~ni.png"
+)
+
+for %%i in (*.m4v) do (
+ffmpeg -i "%%i" "out_frames/%%~ni.png"
+)
+
+for %%i in (*.qt) do (
+ffmpeg -i "%%i" "out_frames/%%~ni.png"
+)
+
+for %%i in (*.mxf) do (
+ffmpeg -i "%%i" "out_frames/%%~ni.png"
+)
+
+for %%i in (*.raw) do (
+ffmpeg -i "%%i" "out_frames/%%~ni.png"
+)
+
+::[FFmpeg video_to_frames_1_0 softy_plug]
+
+if NOT ["%errorlevel%"]==["0"] goto:error
+echo [92m%~n1 Done.[0m
+shift
+if "%~1" == "" goto:end
+goto:again
+:error
+
+echo [93mError! Video files to encode were not found. Check for video files in the folder with the executable .exe file.[0m
+pause
+exit 0
+:end
+
+cls
+echo [92mEncoding succesful. This window will close after 10 seconds.[0m
+timeout /t 10
+
+
+::[FFmpeg video_to_frames_1_0 softy_plug]
---
+++ video_to_frames_1_0_portable/readme.txt
@@ -0,0 +1,10 @@
+╭━━╮╱╭━┳╮╱╱╱╭━╮╱╱╱╱╱╱
+┃━━╋━┫━┫╰┳┳╮┃╋┣╮╭┳┳━╮
+┣━━┃╋┃╭┫╭┫┃┃┃╭┫╰┫┃┃╋┃
+╰━━┻━┻╯╰━╋╮┃╰╯╰━┻━╋╮┃
+╱╱╱╱╱╱╱╱╱╰━╯╱╱╱╱╱╱╰━╯
+softy_plug
+
+video_to_frames_1_0 - utility for create frames images from video files using ffmpeg console
+
+.exe file and repository must be in same directory with video files
